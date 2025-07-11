2295cf51a18ff98c86454d840068883de8298a19
Enricobusatto <enricobusatto75@gmail.com>
2025-07-11 14:20:53 +0200

    aggiunta classe card-hover

--- src/AllTheBooks.jsx
+++ src/AllTheBooks.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import fantasy from './assets/fantasy.json';
+import './AllTheBooks.css';
 console.log(fantasy);
 
 function AllTheBooks() {
@@ -9,7 +10,7 @@ function AllTheBooks() {
             <div className="row g-2">
                 {fantasy.map((book) => (
                     <div className="card card-body col-md-3 h-100 border-0" style={{flex: '0 0 auto'}} key={book.asin}>
-                        <div className="overflow-hidden rounded-4 shadow" style={{ height: '450px' }}>
+                        <div className="overflow-hidden rounded-4 shadow card-hover" style={{ height: '450px' }}>
                             <img
                                 src={book.img}
                                 className="img-fluid w-100 h-100 object-fit-cover "
