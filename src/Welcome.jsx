7479a60f321227d0031915c6f6a4fe7da7a05e7f
Enricobusatto <enricobusatto75@gmail.com>
2025-07-13 13:49:32 +0200

    aggiunto modifica di chiusura e apertura alert

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,20 +1,31 @@
+import { useState } from 'react';
 import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
 import './Welcome.css';
 
 
 function Welcome() {
-  return (
-    <Alert variant="success" id='welcome-alert'>
-      <Alert.Heading>Hey, nice to see you, Welcome to the Books React application!</Alert.Heading>
-      <p>
-        This is a simple app to manage your book collection.
-      </p>
-      <hr />
-      <p className="mb-0">
-        Enjoy your reading journey!
-      </p>
-    </Alert>
-  );
-}
+  const [show, setShow] = useState(true);
 
-export default Welcome;
+  if (show) {
+
+    return (
+      <Alert variant="success" className='shadow' id='welcome-alert' onClose={() => setShow(false)} dismissible>
+        <Alert.Heading>Hey, nice to see you, Welcome to the Books React application!</Alert.Heading>
+        <p>
+          This is a simple app to manage your book collection.
+        </p>
+        <hr />
+        <p className="mb-0">
+          Enjoy your reading journey!
+        </p>
+      </Alert>
+    );
+  }
+      return <Button className='bg-info mt-3 border-0 ' onClick={() => setShow(true)}>Show Alert</Button>;
+  }
+
+
+  export default Welcome;
+
+
